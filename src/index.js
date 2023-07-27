0b358869bf290deb3050cd642d6e5f02e2c918d6
agneta1991 <pupienytea@gmail.com>
2023-07-27 10:13:14 +0300

    linters fixed

--- src/index.js
+++ src/index.js
@@ -8,43 +8,43 @@ const refreshBtn = document.getElementById('refreshBtn');
 const game = 'game';
 
 const getGameId = async () => {
-    if (localStorage.getItem('gameId')) {
-        return localStorage.getItem('gameId');
-    }
-    return createNewGame(game);
+  if (localStorage.getItem('gameId')) {
+    return localStorage.getItem('gameId');
+  }
+  return createNewGame(game);
 };
 
 const gameId = await getGameId();
 
 submitBtn.addEventListener('click', () => {
-    onloadScore(gameId);
+  onloadScore(gameId);
 });
 
 refreshBtn.addEventListener('click', async () => {
-    const scores = await refreshScores(gameId);
+  const scores = await refreshScores(gameId);
 
-    const resultTable = document.getElementById('ul');
-    let child = resultTable.lastElementChild;
-    while (child) {
-        resultTable.removeChild(child);
-        child = resultTable.lastElementChild;
+  const resultTable = document.getElementById('ul');
+  let child = resultTable.lastElementChild;
+  while (child) {
+    resultTable.removeChild(child);
+    child = resultTable.lastElementChild;
+  }
+
+  scores.forEach((individualUser) => {
+    const li = document.createElement('li');
+    li.className = 'listItem';
+    li.innerHTML = `${individualUser.user} : ${individualUser.score}`;
+
+    const isEvenIndex = resultTable.children.length % 2 === 0;
+
+    if (isEvenIndex) {
+      li.style.backgroundColor = 'white';
+    } else {
+      li.style.backgroundColor = '#cdcbcb';
     }
 
-    scores.forEach((individualUser) => {
-        const li = document.createElement('li');
-        li.className = 'listItem';
-        li.innerHTML = `${individualUser.user} : ${individualUser.score}`;
-
-        const isEvenIndex = resultTable.children.length % 2 === 0;
-
-        if (isEvenIndex) {
-            li.style.backgroundColor = 'white';
-        } else {
-            li.style.backgroundColor = '#cdcbcb';
-        }
-
-        resultTable.appendChild(li);
-    });
+    resultTable.appendChild(li);
+  });
 });
 
 export default gameId;
